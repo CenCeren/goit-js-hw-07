7e13c6ca51347b8776b61cc84f820ae18c28b793
Ceren <ceren.krbck@gmail.com>
2025-02-17 17:57:00 +0300

    task2 to change and task 6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById('boxes');
+  
+  boxesContainer.innerHTML = '';
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+
+    const size = 30 + (i * 10);
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = ''; 
+}
+
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const inputField = document.querySelector('input');
+
+createButton.addEventListener('click', () => {
+  const amount = parseInt(inputField.value, 10);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount); 
+    inputField.value = ''; 
+  } else {
+    alert('Please enter a number between 1 and 100.');
+  }
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
